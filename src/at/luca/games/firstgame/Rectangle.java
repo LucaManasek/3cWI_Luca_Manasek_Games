010471dfd34bc66c46283783259e215c4f5b05af
Luca Manasek <luca.manasek@student.htldornbirn.at>
2025-04-09 09:36:03 +0200

    End_of_Lesson

---
+++ src/at/luca/games/firstgame/Rectangle.java
@@ -0,0 +1,27 @@
+package at.luca.games.firstgame;
+
+import org.newdawn.slick.Graphics;
+
+public class Rectangle {
+    private float x;
+    private float y;
+    private float speed;
+    private enum DIRECTION {RIGHT, DOWN, LEFT, UP}
+
+    public Rectangle(int x, int y, float speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public void render(Graphics graphics){
+        graphics.drawRect(this.x, this.y, 10, 10);
+    }
+
+    public void update(int delta){
+        this.x += (float)delta/this.speed;
+        if(this.x > 800) {
+            this.x = 800;
+        }
+    }
+}
